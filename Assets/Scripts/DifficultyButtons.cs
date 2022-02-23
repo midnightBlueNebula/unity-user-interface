7d3a88353fd05a52f348430bec4c4ea0b3704e02
Onur Kaya <onurkaya8@outlook.com>
2022-02-23 15:00:30 +0300

    init project.

---
+++ Assets/Scripts/DifficultyButtons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtons : MonoBehaviour
+{
+    private Button button;
+
+    private GameManager gameManager;
+
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(setDifficulty);
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    private int score = 0;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    public Button restartButton;
+
+    public bool isGameOver;
+
+    public GameObject title;
+
+    // Start is called before the first frame update
+    public void StartGame(int difficulty)
+    {
+        title.SetActive(false);
+
+        UpdateScore(score);
+
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int point)
+    {
+        score += point;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    } 
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
